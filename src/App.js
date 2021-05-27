bd4c69c0049d6c175017353a51eb34cc6816ee98
antonlavrinov <alv.anton.lavrinov@gmail.com>
2021-05-27 16:04:08 +0300

    fourth commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div></div>
+        <div></div>
         <a
           className="App-link"
           href="https://reactjs.org"
